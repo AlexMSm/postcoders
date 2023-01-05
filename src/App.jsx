79ed0cb60ffd506bd73ea21af9dee611720daea3
Alex Smith <109144005+AlexMSm@users.noreply.github.com>
2023-01-05 11:23:51 +0000

    task 3 completed - input form with outcode regex check before a new API call

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,48 @@ import "./App.css";
 
 function App() {
   const [areas, setAreas] = useState([]);
+  const [outcode, setOutcode] = useState("bb12");
+  const [text, setText] = useState("");
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const outcodeRegex = /^[a-zA-Z]{1,2}[0-9][a-zA-Z0-9]?$/;
+    if (text.match(outcodeRegex)) {
+      setOutcode(text);
+    } else {
+      alert("Incorrect outcode");
+    }
+  };
 
   const load = async () => {
     try {
-      const areaData = await getAreaData();
-
+      const areaData = await getAreaData(outcode);
       setAreas(areaData);
     } catch (error) {
-      window.alert("todo: fix app");
+      window.alert(error);
+      setAreas([]);
     }
   };
 
   useEffect(() => {
     load();
-  }, []);
+  }, [outcode]);
 
   return (
     <div className="App">
       <h1>Postcoders</h1>
-      <h2>{`Areas for BB10: ${areas.length}`}</h2>
+      <form className="postcode-form" onSubmit={onSubmit}>
+        <h2>Please enter a valid uk postcode outward code e.g BB12</h2>
+        <input
+          className="postcode-input"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          onSubmit={onSubmit}
+        />
+        <button className="postcode-submit-btn">Submit</button>
+      </form>
+
+      <h2>{`Areas for ${outcode}: ${areas.length}`}</h2>
     </div>
   );
 }
